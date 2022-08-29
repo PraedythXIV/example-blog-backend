a7f3130517eec36647a4d67086f8a6273ea0444d
77011-32-18 <77011-32-18@exchange.ad.afpanet>
2022-08-29 15:31:32 +0200

    chore: initial commit
    - npm deps
    - eslint, etc.
    - initial schema migration

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # BlogTest
+
+# # process.env
+
+```
+DB_USER=
+DB_HOST=
+DB_PORT=
+DB_PASSWORD=
+DB_DATABASE=
+```
+
+Additionally, add any
+`DB_*` (e.g: password,host, ...) env vars as needed.
---
+++ knexfile.js
@@ -0,0 +1,25 @@
+import "dotenv/config"
+
+export default {
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+  migrations: {
+    directory: "./src/db/migrations",
+    stub: "./src/db/migrations.stub",
+  },
+}
+
+/*console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+})
+*/
