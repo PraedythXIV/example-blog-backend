16ee312d60065a2cb185035daf79b6bdbe4e2e51
77011-32-18 <77011-32-18@exchange.ad.afpanet>
2022-08-30 12:37:26 +0200

    chore: initial commit feat: adding validators and POST /users
    - npm deps
    - eslint, etc.
    - initial schema migration

---
+++ src/config.js
@@ -0,0 +1,31 @@
+import "dotenv/config"
+
+const config = {
+  server: {
+    port: process.env.SERVER_PORT,
+  },
+  db: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: "./src/db/migrations",
+      stub: "./src/db/migration.stub",
+    },
+  },
+}
+
+/*console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+})
+*/
+
+export default config
